61e2aa271e449a14923941fbcdf56bcff742dafd
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2024-10-29 16:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const byteSize = (str) => {
-  // write your code here
     return new Blob([str]).size;
-
+};
 
 // Examples
 console.log(byteSize('hello world')); // Output: 11
 console.log(byteSize('안녕하세요')); // Output: 15
-console.log(byteSize('')); 
-};
+console.log(byteSize('')); // Output: 0
 
 // Do not change the code below
 const str = prompt("Enter some string.");
